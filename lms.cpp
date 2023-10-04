f64ce36aee955233e55175392bbc9118af38bcb6
kewadahal <110725776+ProfKdX7@users.noreply.github.com>
2023-10-04 14:46:20 +0545

    initialized viewcategory function and modified otger classes

--- lms.cpp
+++ lms.cpp
@@ -17,7 +17,7 @@ void clear(){
 class Book
 {
 public:
-    Book(const string &title, const string &author) : title(title), author(author) {}
+    Book(const string &title, const string &author, const string &category) : title(title), author(author), category(category) {}
 
     const string &getTitle() const
     {
@@ -29,9 +29,15 @@ public:
         return author;
     }
 
+    const string &getCategory() const
+    {
+        return category;
+    }
+
 private:
     string title;
     string author;
+    string category;
 };
 
 class Library
@@ -39,7 +45,7 @@ class Library
 public:
     void addBook(const string &title, const string &author, const string &category)
     {
-        books.push_back(Book(title, author));
+        books.push_back(Book(title, author, category));
         updateCategoryFile(category, title, author);
     }
 
@@ -61,13 +67,18 @@ public:
         cout << "Library Inventory:\n";
         for (const auto &book : books)
         {
-            cout << "Title: " << book.getTitle() << ", Author: " << book.getAuthor() << "\n";
+            cout << "Title: " << book.getTitle() << ", Author: " << book.getAuthor() << ", Category: " << book.getCategory() << "\n";
         }
     }
 
     void displayRecords() const
     {
-        //for displaying records
+        // for displaying records
+    }
+
+    const vector<Book> &getBooks() const
+    {
+        return books;
     }
 
 private:
@@ -113,6 +124,7 @@ private:
         rename((category + "_temp.txt").c_str(), (category + ".txt").c_str());
     }
 };
+
 
 class User
 {
@@ -257,19 +269,94 @@ public:
             switch (choice)
             {
             case 1:
-                //viewing category
+                viewCategory(library);
                 break;
             case 2:
-                //borrowing a book
+                // Implement the logic for borrowing a book here
                 break;
             case 3:
-                //returning a book
+                // Implement the logic for returning a book here
                 break;
             case 4:
                 return;
             default:
                 cout << "Invalid choice. Try again.\n";
             }
+        }
+    }
+
+    void viewCategory(const Library &library)
+    {
+        cout << "\nCategories List:\n";
+        cout << "1. Story\n";
+        cout << "2. Poetry\n";
+        cout << "3. Course Book\n";
+        cout << "4. Novel\n";
+        cout << "5. Drama\n";
+        cout << "Enter your choice: ";
+        int choice;
+        cin >> choice;
+
+        string category;
+        switch (choice)
+        {
+        case 1:
+            category = "Story";
+            break;
+        case 2:
+            category = "Poetry";
+            break;
+        case 3:
+            category = "Course Book";
+            break;
+        case 4:
+            category = "Novel";
+            break;
+        case 5:
+            category = "Drama";
+            break;
+        default:
+            cout << "Invalid category choice.\n";
+            return;
+        }
+
+        // Call a function to display books in the selected category
+        displayBooksInCategory(library, category);
+
+        // Display the content of the category file
+        displayCategoryFileContent(category);
+    }
+
+    void displayBooksInCategory(const Library &library, const string &category)
+    {
+        cout << "\nBooks in Category '" << category << "':\n";
+        // Iterate through the library's books and display those in the selected category
+        for (const auto &book : library.getBooks())
+        {
+            // Check if the book belongs to the selected category
+            if (book.getCategory() == category)
+            {
+                cout << "Title: " << book.getTitle() << ", Author: " << book.getAuthor() << "\n";
+            }
+        }
+    }
+
+    void displayCategoryFileContent(const string &category) const
+    {
+        ifstream file(category + ".txt");
+        if (file.is_open())
+        {
+            cout << "\nContent of Category '" << category << "':\n";
+            string line;
+            while (getline(file, line))
+            {
+                cout << line << "\n";
+            }
+            file.close();
+        }
+        else
+        {
+            cout << "Category file not found.\n";
         }
     }
 };
